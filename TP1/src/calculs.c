c1c139e28c84af2acee75df2d8e4e1007e4ae379
Pierre TAJAN <tamotaki-pierro@outlook.com>
2025-11-21 11:12:18 +0100

    Implement basic calculator with multiple operations

--- TP1/src/calculs.c
+++ TP1/src/calculs.c
@@ -1,1 +1,46 @@
 
+
+#include <stdio.h>
+
+int main() {
+    int num1 = 10;
+    int num2 = 3;
+    char op = '%'; // changer l'opérateur pour tester
+
+    switch(op) {
+        case '+':
+            printf("%d + %d = %d\n", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%d - %d = %d\n", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%d * %d = %d\n", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if(num2 != 0)
+                printf("%d / %d = %d\n", num1, num2, num1 / num2);
+            else
+                printf("Division par 0 impossible\n");
+            break;
+        case '%':
+            if(num2 != 0)
+                printf("%d %% %d = %d\n", num1, num2, num1 % num2);
+            else
+                printf("Modulo par 0 impossible\n");
+            break;
+        case '&':
+            printf("%d & %d = %d\n", num1, num2, num1 & num2);
+            break;
+        case '|':
+            printf("%d | %d = %d\n", num1, num2, num1 | num2);
+            break;
+        case '~':
+            printf("~%d = %d\n", num1, ~num1);
+            break;
+        default:
+            printf("Opérateur inconnu\n");
+    }
+
+    return 0;
+}
