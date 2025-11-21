7db5d619a1111fed7fd17642a34a4908dfc9d062
Pierre TAJAN <tamotaki-pierro@outlook.com>
2025-11-21 10:40:24 +0100

    Simplify boucles.c by removing while loop
    Removed the while loop implementation and kept only the for loop version.

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int compteur = 5; // changer cette valeur pour tester différentes tailles
+    int compteur = 5; // changer cette valeur (<10) pour tester différentes tailles
     int i, j;
 
-    printf("Version avec boucle for :\n");
     for (i = 1; i <= compteur; i++) {
         for (j = 1; j <= i; j++) {
             if (i % 2 == 0 && j % 2 == 0) {
@@ -14,22 +13,6 @@ int main() {
             }
         }
         printf("\n");
-    }
-
-    printf("\nVersion avec boucle while :\n");
-    i = 1;
-    while (i <= compteur) {
-        j = 1;
-        while (j <= i) {
-            if (i % 2 == 0 && j % 2 == 0) {
-                printf("# ");
-            } else {
-                printf("* ");
-            }
-            j++;
-        }
-        printf("\n");
-        i++;
     }
 
     return 0;
