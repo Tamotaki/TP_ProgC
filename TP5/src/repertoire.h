1563d5961e0754049379fc55aef04006a7f24de7
Pierre TAJAN <tamotaki-pierro@outlook.com>
2025-12-01 15:43:04 +0100

    Update repertoire.h

---
+++ TP5/src/repertoire.h
@@ -0,0 +1,8 @@
+#ifndef REPERTOIRE_H
+#define REPERTOIRE_H
+
+void lire_dossier(const char *nom_repertoire);          /* 5.1 */
+void lire_dossier_recursif(const char *nom_repertoire); /* 5.2 */
+void lire_dossier_iteratif(const char *nom_repertoire); /* 5.3 */
+
+#endif
