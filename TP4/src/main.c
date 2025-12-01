ae70a7edc4f0780eacfcb240cfecf1687f39df42
Pierre TAJAN <tamotaki-pierro@outlook.com>
2025-12-01 13:54:24 +0100

    Update main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include "operator.h"
+#include "fichier.h"
+#include "liste.h"
 
+/* Protos des fonctions d'entrée */
+void exercice_41(void);
+void exercice_42(void);
+void exercice_47(void);
+
+int main(void) {
+    int choix = 0;
+
+    printf("Choisir l'exercice a executer :\n");
+    printf("1 - Exercice 4.1 (operateurs)\n");
+    printf("2 - Exercice 4.2 (fichiers)\n");
+    printf("3 - Exercice 4.7 (liste de couleurs)\n");
+    printf("Votre choix : ");
+    scanf("%d", &choix);
+
+    switch (choix) {
+        case 1:
+            exercice_41();
+            break;
+        case 2:
+            exercice_42();
+            break;
+        case 3:
+            exercice_47();
+            break;
+        default:
+            printf("Choix invalide.\n");
+            break;
+    }
+
+    return 0;
+}
