51ed110f9c23540d93eee031946b485a341107b4
Pierre TAJAN <tamotaki-pierro@outlook.com>
2025-12-01 13:56:46 +0100

    Update 4.2 main.c

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,3 +1,17 @@
+void exercice_41(void) {
+    int num1, num2;
+    char op;
+
+    printf("Entrez num1 : ");
+    scanf("%d", &num1);
+    printf("Entrez num2 : ");
+    scanf("%d", &num2);
+    printf("Entrez l'operateur (+, -, *, /, %%, &, |, ~) : ");
+    scanf(" %c", &op);
+
+    int res = calculer(num1, num2, op);
+    printf("Resultat : %d\n", res);
+}
 #include <stdio.h>
 #include "operator.h"
 #include "fichier.h"
